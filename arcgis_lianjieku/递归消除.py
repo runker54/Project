66873465f0780d631340bd0084d798c7dcd7d442
runker54 <runkr54@gmail.com>
2020-09-17 09:36:07 +0800

    添加了打印内容

--- arcgis_lianjieku/递归消除.py
+++ arcgis_lianjieku/递归消除.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-#
 # -------------------------------------------------------------------------------
-# Author:       A
+# Author:       Runker54
 # Date:         2020-09-05
 # -------------------------------------------------------------------------------
 import arcpy
 import time
 
-arcpy.env.workspace = r"C:\Users\65680\Desktop\TEST\TEST.gdb"
+arcpy.env.workspace = r"E:\台账\湄潭县\数据库\TEST.gdb"
 
 lbbm_list = []
-with arcpy.da.SearchCursor("test", ["LBBM"]) as currsor:
+with arcpy.da.SearchCursor("T新南镇_消除后_标识", ["LBBM"]) as currsor:
     for row in currsor:
         lbbm_list.append(row[0])
 lbbm_list = list(set(lbbm_list))
-layers_1 = arcpy.MakeFeatureLayer_management("test", "testlyr")  # 创建总视图
+layers_1 = arcpy.MakeFeatureLayer_management("T新南镇_消除后_标识", "testlyr")  # 创建总视图
+# 个数统计
+total_count = len(lbbm_list)
+count_total = 1
 for one_elimte in lbbm_list:
     arcpy.env.overwriteOutput = True
-    print(one_elimte)
+    # print(one_elimte)
     query = '\"LBBM\"=\'%s\' AND \"Shape_Area\" < 100' % one_elimte
     arcpy.SelectLayerByAttribute_management(layers_1, 'NEW_SELECTION', query)
     arcpy.Eliminate_management(layers_1, "testA", "LENGTH", '\"LBBM\" <> \'%s\'' % one_elimte)  # 存储为A
@@ -27,9 +30,10 @@ for one_elimte in lbbm_list:
         print("NONE")
     arcpy.SelectLayerByAttribute_management(layers_1, 'NEW_SELECTION', query)
     arcpy.Eliminate_management(layers_1, "testB", "LENGTH", '\"LBBM\" <> \'%s\'' % one_elimte)  # 利用A存储为B
+    count_total += 1
     layers_1 = arcpy.MakeFeatureLayer_management("testB", "testlyr")  # 利用B创建视图
     arcpy.Delete_management("testA")  # 删除A
-    print(f"{one_elimte}消除成功")
+    print(f"{one_elimte}消除成功,已消除{count_total}个，剩余{total_count-count_total}个，共{total_count}个")
 
 
 
