6417a53b36973dce304282eeb4c431e370c5346d
Runker54 <59903664+runker54@users.noreply.github.com>
2023-03-15 15:40:22 +0800

    Delete DOCMENT_CHULI directory

--- DOCMENT_CHULI/DOC_ELE.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import docx
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-doc = docx.Document()
-paragraph = doc.add_paragraph()
-paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-run = paragraph.add_run("")
-run.add_picture(r'C:\Users\65680\Desktop\新建文件夹\520425000686.jpg')
-doc.save(r"C:\Users\65680\Desktop\DOC\ccc.docx")
