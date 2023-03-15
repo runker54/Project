935d1d3426e6eb653fff374042d83427403ac346
Runker54 <59903664+runker54@users.noreply.github.com>
2023-03-15 15:40:50 +0800

    Delete 草稿 directory

--- 草稿/文件夹重命名.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-import os
-path = r""
-old_name = os.listdir(path)
-for one_name in old_name:
-    new_name = ''
-    os.rename(os.path.join(path, old_name), os.path.join(path, new_name))
